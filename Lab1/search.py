8c531ac4292366a152a0e1ae50493c36ed6d7b7c
acharyabibash <acharabibash@outlook.com>
2023-03-01 19:05:10 +0545

    Recursive Binary Search Implemented in search.py for Lab 1

---
+++ Lab1/search.py
@@ -0,0 +1,29 @@
+#linear search
+def linear_search(values,target):
+    n = len(values)
+    for i in range(n):
+        if values[i] == target:
+            return i
+    return -1
+
+#recursive binary search
+def binarySearchRecursive(arr,low,high,x):
+    if  high >= low:
+        mid = (high + low) // 2 # floor division for eg 7//2 = 2
+        if arr[mid]==x:
+            return mid
+        elif arr[mid] > x:
+            return binarySearchRecursive(arr,low,mid-1,x)
+        
+        else:
+            return binarySearchRecursive(arr,mid+1,high,x)
+        
+    return -1
+
+
+arr = [2,3,4,10,1]
+x=2
+result = binarySearchRecursive(arr,0,len(arr)-1,x)
+print(result)
+
+
